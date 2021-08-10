77729d11140c8599bd91337aac496892749f8713
moonhy7 <moonhy7@naver.com>
2021-08-11 01:00:24 +0900

    21.8.11.Wed

--- BaekJoon_Algorithm/src/geometry/PlayGround.java
+++ BaekJoon_Algorithm/src/geometry/PlayGround.java
@@ -8,7 +8,7 @@ public class PlayGround {
 		int d1 = sc.nextInt();
 		int d2 = sc.nextInt();
 		double pi = 3.141592;
-		
+		 
 		double round = 2 * d1 + 2 * d2 * pi;
 		
 		System.out.println(round);
